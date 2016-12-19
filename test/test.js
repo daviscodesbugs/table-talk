b72e1cf74557bf0ef1c05870a0f266302bf6a5bb
daviscodesbugs <daviscodesbugs@gmail.com>
2016-12-19 08:29:55 -0700

    Set up some test cases

--- test/test.js
+++ test/test.js
@@ -1,23 +1,60 @@
 const assert = require('assert');
 const fs = require('fs');
 const lr = require('readline');
+const read = require('fs-readdir-recursive');
+const filepath = require('path');
+const lint = require('jsonlint');
 
-reader = lr.createInterface({
-	input: fs.createReadStream('../family.csv')
-});
-reader.on('line', function (line) {
-	assert(line.toLowerCase() == line);
-	console.log(line);
+fileNames = read('./src/cards');
+jsonFileNames = fileNames.filter(function (x) {
+	return filepath.extname(x) === '.json';
 });
 
-describe('Syntax', function () {
-	reader = lr.createInterface({
-		input: fs.createReadStream('family.csv')
-	});
-	reader.on('line', function (line) {
-		it('all lower case', function () {
-			assert(line.toLowerCase() == line);
-			console.log(line);
+String.prototype.replaceAll = function (search, replacement) {
+	var target = this;
+	return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+describe('JSON Lint', function () {
+	jsonFileNames.forEach(function (path) {
+		it(path, function () {
+			lint.parse(readJson(path)); // throws if error found
 		});
 	});
 });
+
+describe('Syntax', function () {
+	jsonFileNames.forEach(function (path) {
+		it(path, function () {
+			var json = JSON.parse(readJson(path));
+
+			// Lowercase
+			json['questions'].forEach(function (q) {
+				assert(newToLowerCase(q) == q,
+					"\n" + q + "\n" + newToLowerCase(q));
+			});
+
+			assert(json['count'] == json['questions'].length,
+				"count field (" + json['count'] + ") != actual length (" + json['questions'].length + ")");
+		});
+	});
+});
+
+function readJson(path) {
+	return fs.readFileSync('./src/cards/' + path).toString();
+}
+
+const properNouns = [
+	"TV",
+	"Olympics",
+	"'A's",
+	"God",
+	"Gold Rush"
+];
+function newToLowerCase(str) {
+	str = str.toLowerCase();
+	properNouns.forEach(function (noun) {
+		str = str.replaceAll(noun.toLowerCase(), noun);
+	});
+	return str;
+}
