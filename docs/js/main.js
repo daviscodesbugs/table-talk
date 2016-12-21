0523cac961ddfbcaedd18b179ca1979a17e85b16
daviscodesbugs <daviscodesbugs@gmail.com>
2016-12-20 21:11:58 -0700

    Slide cards in and load questions

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,35 @@
 var app = angular.module("myApp", []);
 
-app.controller('myCtrl', function ($scope) {
+app.controller('myCtrl', function ($scope, $http) {
     $scope.test = "Family";
+
+    $scope.questions = [];
+
+    $scope.selectDeck = function (deck) {
+        switch (deck) {
+            case 'family':
+                $http({
+                    method: 'GET',
+                    url: 'https://raw.githubusercontent.com/daviscodesbugs/table-talk/master/src/cards/family.json'
+                }).then(function success(res) {
+                    $scope.questions = res.data.questions;
+                }, function error(res) {
+                    alert('Couldn\'t get family questions');
+                });
+                break;
+            default:
+                alert('nothing');
+        }
+    }
+});
+
+
+$(function () {
+    setTimeout(function () {
+        $('#family_card').removeClass('off-bottom');
+    }, 500);
+
+    $('#family_card').click(function () {
+        $(this).addClass('off-top');
+    });
 });
